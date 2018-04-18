bd56a4d4d98784b2b8198fdd5b98db38e7a785a0
Alex Chirkin <ihoooly@gmail.com>
2018-04-18 13:47:38 +0300

    asd

---
+++ portfolio/1-5/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#sticker").sticky({topSpacing:25});
+
+
+$("a[href='#embodiment']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, 1000);
+  return false;
+});
+
+
+  });
+
+
+
+  
+
+
